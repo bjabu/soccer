5524ddf7f496cce9575a69d4e1451bd09e484d2b
Bjarne Bülow <bjarne.bulow@xlent.se>
2018-05-30 14:53:15 +0200

    First version of the test

--- README.md
+++ README.md
@@ -3,9 +3,8 @@
 > A simple setup of a backend server setup in node.js serving soccer data from a local csv-file to a single page frontend app in Vue. The server provides a REST API allowing for:
 
 
-* 
-*
-*
+* Retreiving a list of players
+* Retrieving statistics for a selected player
  
 
 ## Build Setup
@@ -21,4 +20,10 @@ npm run dev
 npm run build
 ```
 
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+> The server setup should be cloned from [this repository](https://github.com/bjabu/frontendtest-node). Both server and client servers should be started.
+
+
+### Comments
+* I did choose **Vue** as it is convenient for small seetups and I have a decent knowledge.
+* Initial comments mentioned porting server to Java. I did choose to see that as a typo and kept **node.js** as it would at least have taken me a workday. I have not been close to Java coding for fifteen years (though I use the IntelliJ editor).
+* **Limitations:**  No decent error handling. 
--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,43 @@
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Players</h2>
-    <ul class="playerlist" v-if="posts && posts.length">
-      <li v-for="player in this.posts" v-bind:key="player.id" @click="populate(player.id)">
-        {{player.name}}
-      </li>
-    </ul>
+    <div class="container">
+      <ul class="playerlist" v-if="posts && posts.length">
+        <li v-for="player in this.posts" v-bind:class="{active:isActive===player.id}" v-bind:key="player.id" @click="playerData(player.id)">
+          {{player.name}}
+        </li>
+      </ul>
+      <div class="playerdata">
+        <div class="player"  v-if="playerStatistics.data && playerStatistics.data.length">
+          <b>Name:</b> {{playerStatistics.data[0].name}}<br>
+          <b>Ranking:</b> {{playerStatistics.data[0].ranking}}<br>
+          <b>Klubb:</b> {{playerStatistics.data[0].teamName}}<br>
+          <b>Liga:</b> {{playerStatistics.data[0].tournamentName}}<br>
+          <b>Ålder:</b> {{playerStatistics.data[0].age}}<br>
+          <b>Vikt:</b> {{playerStatistics.data[0].weight}}<br>
+          <b>Längd:</b> {{playerStatistics.data[0].height}}<br>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import axios from 'axios'
   export default {
-   name: 'app',
+    name: 'app',
     methods: {
+      playerData: function (id) {
+        this.isActive = id;
+        axios.get(`//localhost:3000/api/player/${id}`)
+          .then(response => {
+            this.playerStatistics = response;
+          })
+          .catch(e => {
+            alert(`Error reading player data from server!`);
+            // this.errors.push(e)
+          })
+      },
       populate: function() {
         alert(JSON.stringify(this.posts));
       }
@@ -25,6 +49,8 @@
       msg: 'Soccer statistics',
       // posts: {id:'',name:''}
       posts: {},
+      playerStatistics: {},
+      isActive: '',
       errors: ''
     }
    },
@@ -35,22 +61,24 @@
         this.posts = response.data
       })
       .catch(e => {
-        this.errors.push(e)
+        alert('Error reading players from server! Perhaps server not started?');
+        // this.errors.push(e)
       })
-
-    // async / await version (created() becomes async created())
-    //
-    // try {
-    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
-    //   this.posts = response.data
-    // } catch (e) {
-    //   this.errors.push(e)
-    // }
   }
 }
 </script>
 
 <style lang="scss">
+  .container {
+    display: flex;
+  }
+  .playerdata {
+    width: 300px;
+    margin: 0 auto;
+  }
+  .player {
+    text-align: left;
+  }
   ul.playerlist {
     border: 1px solid darkgrey;
     width: 200px;
@@ -61,7 +89,12 @@
       line-height: 1.5;
       cursor:pointer;
       &:hover {
-        color: blue;
+        font-weight: 600;
+        color: green;
+      }
+      &.active {
+        color: green;
+        text-decoration: underline;
         font-weight: 600;
       }
     }
